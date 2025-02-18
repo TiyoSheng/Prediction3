4242a46eb0f5b397c86207c4a288b54129db2280
Tiyo <tiyosheng@gmail.com>
2025-02-18 22:40:29 +0800

    feat: add Monad Testnet

--- src/config.js
+++ src/config.js
@@ -1,5 +1,14 @@
 const conf = {
   networks: {
+    10143: {
+      token: "MON",
+      chainId: "0x279f",
+      chainName: "Monad Testnet",
+      token_list: {
+        PT: "0x22B610a109F5eD204D5D25e186940cb722054988"
+      },
+      factory_address: "0x0188b5b4D6869a706c153b89B7142Dd4879B6CA1",
+    },
     11155111: {
       token: "ETH",
       chainId: "0xaa36a7",
